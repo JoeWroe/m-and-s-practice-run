079dfa0febbf40fef4cc81a3df2e1e1031a17876
JoeWroe <joe_wroe@icloud.com>
2016-07-22 11:45:33 +0100

    Add User Story One

---
+++ README.md
@@ -0,0 +1,8 @@
+##User Stories
+
+**User Story One:**
+```
+As a customer,
+So that I know what products are avliable to put in the basket,
+I'd like the basket to know what's on the product catalog.
+```
---
+++ spec/feature/user_story_spec.rb
@@ -0,0 +1,13 @@
+describe "User Stories" do
+
+  describe "User Story One" do
+
+    it
+      "As a customer,
+      So that I know what products are avliable to put in the basket,
+      I'd like the basket to know what's on the product catalog." 
+    do
+
+    end
+  end
+end
