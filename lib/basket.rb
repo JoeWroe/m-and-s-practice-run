e7eb32b8b899575f232e26b5f2355cf2b250bbd6
JoeWroe <joe_wroe@icloud.com>
2016-07-22 12:15:16 +0100

    Setup basket to init with product_catalog
    ∆ Add product_catalog as an init ardument to basket. This injects a
    product catalog.
    
    ∆ Add method that calls that product catalog.

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,3 +1,15 @@
 class Basket
 
+  def initialize(product_catalog: )
+    @product_catalog = product_catalog
+  end
+
+  def avaliable_products
+    product_catalog
+  end
+
+  private
+
+  attr_reader :product_catalog
+
 end
--- spec/feature/user_story_spec.rb
+++ spec/feature/user_story_spec.rb
@@ -6,9 +6,9 @@ describe "User Stories" do
   describe "User Story One" do
 
     it "As a customer,
-        So that I know what products are avliable to put in the basket,
-        I'd like the basket to know what's on the product catalog." do
-          expect(basket.avaliable_products).to eq(product_catalog)
+    So that I know what products are avliable to put in the basket,
+    I'd like the basket to know what's on the product catalog." do
+      expect(basket.avaliable_products).to eq(product_catalog)
     end
   end
 end
